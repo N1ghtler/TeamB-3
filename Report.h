283e29d7a53e1c0276efe00731367dcf29274061
N1ghtler <beevadhana@gmail.com>
2021-12-27 20:44:20 +0700

    Report day

--- Report.h
+++ Report.h
@@ -8,3 +8,21 @@ void WriteData(int Date,int Mouth,int year){
      Write<<Date<<" "<<Mouth<<" "<<year<<" "<<endl;
      Write.close();
 }
+
+void Report_Day(int Day,int Month,int Year){
+     int Report_Date,Report_Month,Report_Year;
+     fstream Read;
+     Read.open("Data.txt",ios::in);
+     while (!Read.eof())
+     {
+          Read>>Report_Date>>Report_Month>>Report_Year;
+
+          if (Report_Date == Day || Report_Month == Month || Report_Year == Year)
+          {
+               /* code */
+          }
+          
+     }
+     Read.close();
+     cout << "Total ...... " << "Total ......  " << "Total ......  " << "Total ......  "<< "Total ......  ";
+}
